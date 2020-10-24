be23bcb639e7443a2134f4d58e7cc499a3634ecc
Daniel Layland <laylandwork@gmail.com>
2020-10-24 15:49:50 +0100

    Added class diagrams and readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Super Service (v2.00)
--- SuperService_MVC/Models/ItemViewModel.cs
+++ SuperService_MVC/Models/ItemViewModel.cs
@@ -11,13 +11,6 @@ namespace SuperService_MVC.Models
     {
         public IList<SelectListItem> IngredientsDropDown { get; set; }
         public IList<string> IngredientsSelected { get; set; }
-        public string CostAsCurrency => Cost.ToString("C");
-        public string FormattedCalories => $"{Calories}kcal";
-        public string FormattedProtein => $"{Math.Round(Protein, 2)}g";
-        public string FormattedFat => $"{Math.Round(Fat, 2)}g";
-        public string FormattedSugar => $"{Math.Round(Sugar, 2)}g";
-        public string FormattedCarbs => $"{Math.Round(Carbohydrates, 2)}g";
-        public string FormattedSalt => $"{Math.Round(Salt, 2)}g";
         public bool CanOrder => ItemIngredients.Where(x => x.Ingredient.NumberInStock > 0).Count() == ItemIngredients.Count();
         public int Calories => ItemIngredients.GroupBy(x => x.Item.ItemID).Select(x => x.Sum(x => x.Ingredient.Calories)).Sum();
         public double Protein => ItemIngredients.GroupBy(x => x.Item.ItemID).Select(x => x.Sum(x => x.Ingredient.Protein)).Sum();
